473b8c39113e1d4dfa89c688ba36d1f9c50d98fc
Sheddy00 <hei.sheddy.2@gmail.com>
2023-11-20 00:48:44 +0400

    main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import Model.Books;
+
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     private static Connection connection;
@@ -21,6 +25,20 @@ public class Main {
             }
         }
         return connection;
+    }
+    private static <BooksCrudOperations> void testBooksOperations(BooksCrudOperations booksCrudOperations) {
+        // Créez des objets Books pour les tests
+        List<Books> booksToSave = new ArrayList<>();
+        booksToSave.add(new Books("Book1", 200, "COMEDY", "2023-01-01", "Author1"));
+        booksToSave.add(new Books("Book2", 150, "ROMANCE", "2023-02-01", "Author2"));
+
+        // Testez l'opération saveAll
+        List<Books> savedBooks = booksCrudOperations.saveAll(booksToSave);
+        System.out.println("Saved Books: " + savedBooks);
+
+        // Testez l'opération findAll
+        List<Books> allBooks = booksCrudOperations.findAll();
+        System.out.println("All Books: " + allBooks);
     }
     public static void main(String[] args) throws Exception{
         System.out.println("Hello world!");
