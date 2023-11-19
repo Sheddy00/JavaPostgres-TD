93755c3d27e3a53ceba59ab7a42fcd58640e2f02
Sheddy00 <hei.sheddy.2@gmail.com>
2023-11-19 12:12:49 +0400

    Connection to postgres

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+    private static Connection connection;
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                String url = System.getenv("DB_URL");
+                String userName = System.getenv("DB_USERNAME");
+                String password = System.getenv("DB_PASSWORD");
+                if (url == null || userName == null || password == null) {
+                    throw new IllegalStateException("Please define the url, username and password in the environment variable");
+                }
+                connection = DriverManager.getConnection(url, userName, password);
+                System.out.println("Connection réussi !!!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        getConnection();
     }
 }
